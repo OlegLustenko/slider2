44d0c1027b9db010ae3e37b2a824d05d92216636
Oleg Lustenko <oleglustenko@gmail.com>
2017-01-31 18:07:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,42 +8,50 @@ let index;
 
 
 let images = document.querySelectorAll('.carousel>li');
+const img = ['img/img1.jpg', 'img/img2.jpg', 'img/img3.jpg'];
 let carousel = document.getElementById('carousel');
-let first = carousel.firstElementChild.cloneNode(true);
-first.classList.add('clone');
-first.classList.remove('active');
-let last = carousel.lastElementChild.cloneNode(true);
-last.classList.add('clone');
-carousel.prepend(last);
-carousel.append(first);
 
-
+let number = 748;
+let xxx = 1;
 
 btnRight.onclick = () => {
+
+  let active = document.querySelector('.active');
+
+  if (xxx >= 3) {
+    console.log(carousel);
+    const prev = carousel.lastElementChild.previousElementSibling.innerHTML = `<img src="${img[carousel.children.length-1]}">`
+    carousel.lastElementChild.innerHTML = `<img src="${img[carousel.children.length - 3]}">`
+    carousel.style.transform = `translateX(-${number - 748 * 2}px)`;
+    carousel.style.transitionDuration = ''
+    // carousel.style.transitionDuration = '.2s';
+
+    setTimeout(function() {
+      carousel.style.transform = `translateX(-${number - 748}px)`;
+      carousel.style.transitionDuration = '.2s';
+
+    }, 0);
+
+    return;
+  }
+  xxx++
+
+
   for (let i = 0; i < images.length; i++) {
     if (images[i].classList.contains('active')) {
       activeImg = images[i];
       index = i;
-    } else {
-      continue
     }
   }
 
   let nextImg = activeImg.nextElementSibling;
-  let number = ((index + 2) * 748);
 
-    activeImg.classList.remove('active');
-    carousel.style.transform = `translateX(-${number}px)`;
-    carousel.style.transitionDuration = '.5s'
-    nextImg.classList.add('active');
-    
-    if (nextImg.classList.contains('clone')) {   
-      nextImg = carousel.firstElementChild.nextElementSibling;
-      nextImg.classList.add('active');
-  carousel.style.transform = `translateX(-748px)`;
-    carousel.style.transitionDuration = '.0s'
-      
-  }
+  activeImg.classList.remove('active');
+  carousel.style.transform = `translateX(-${number}px)`;
+  carousel.style.transitionDuration = '.2s'
+  nextImg.classList.add('active');
+  number += 748
+
 }
 
 
